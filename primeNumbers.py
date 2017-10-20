ffedfbb4a3458a69df740cc34f5d5e76d0d13de4
gtsofa <jlsnyule@yahoo.com>
2017-10-20 16:17:30 +0300

    refactored prime function

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,2 +1,18 @@
+def primeNumbers(number):
+    number_list = []
+    if type(number) != int:
+        raise TypeError
+    elif number <= 0:
+        return number_list
+    else:
+        for i in range(2, number + 1):
+            is_prime = True
+            for j in range(2, int(i ** 0.5) + 1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                number_list.append(i)
 
-def primeNumbers(number)
+        return number_list
+print(primeNumbers(10))
